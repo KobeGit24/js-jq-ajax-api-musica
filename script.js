bf64849e77fc541dd9cc45c2de0a4e5e4100c457
Stefano <stefano.zaccaria29@gmail.com>
2020-07-22 18:16:27 +0200

    ex complete!

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
 // GOAL: Attraverso una chiamata ajax all'Api di boolean avremo a disposizione una decina di dischi musicali. Servendoci di handlebars stampiamo tutto a schermo. In questo momento non è importante la parte grafica. Bonus: Creare una select con i seguenti generi: pop, rock, metal e jazz. In base a cosa scegliamo nella select vedremo i corrispondenti cd. Chiamata: https://flynn.boolean.careers/exercises/api/array/music Layout base: https://bitbucket.org/booleancareers/ex-dischi-musicali-layout
 
 function init() {
-    // var select = $('#select');
-    // select.trigger('change');
-    // select.change(function () {
-    //     var data = $(this).val();
-    //     console.log(data);
-    // });
     callApi();
 }
 
@@ -20,32 +14,22 @@ function callApi() {
             var compiled = Handlebars.compile(template);
             var target = $('#target');
             var select = $('#select');
-            select.trigger('change');
 
             var response = data.response;
             var success = data.success;
             if (success) {
                 for (var i = 0; i < response.length; i++) {
-                    var element = response[i];
-                    var musicObj = compiled({
-                        'poster': element.poster,
-                        'author': element.author,
-                        'title' : element.title,
-                        'year' : element.year
-                    });
+                    var musicObj = compiled(response[i]);
                     target.append(musicObj);
                 }
                 select.change(function () {
                     var data = $(this).val();
-                    for (var i = 0; i < response.length; i++) {
-                        var x = response[i].genre;
-                        y = x.toLowerCase();
-                        console.log(x);
-                        if (y === data) {
-                            element.show();
-                        } else {
-                            element.hide();
-                        }
+                    $('.cds-container>.cd').hide();
+                    if (data != "") {  
+                        $('.cds-container .cd[data-genere="'+ data +'"]').show();
+                        console.log($('.cds-container .cd [data-genere="'+ data +'"]'));
+                    } else {
+                        $('.cds-container>.cd').show();
                     }
                 });
             } else {
