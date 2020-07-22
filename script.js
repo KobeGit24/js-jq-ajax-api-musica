1bd09855331aad73e13bd98a3931482320b47465
Stefano <stefano.zaccaria29@gmail.com>
2020-07-22 16:53:50 +0200

    ex complete!

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 // GOAL: Attraverso una chiamata ajax all'Api di boolean avremo a disposizione una decina di dischi musicali. Servendoci di handlebars stampiamo tutto a schermo. In questo momento non è importante la parte grafica. Bonus: Creare una select con i seguenti generi: pop, rock, metal e jazz. In base a cosa scegliamo nella select vedremo i corrispondenti cd. Chiamata: https://flynn.boolean.careers/exercises/api/array/music Layout base: https://bitbucket.org/booleancareers/ex-dischi-musicali-layout
 
 function init() {
+    // var select = $('#select');
+    // select.trigger('change');
+    // select.change(function () {
+    //     var data = $(this).val();
+    //     console.log(data);
+    // });
     callApi();
 }
 
@@ -13,10 +19,11 @@ function callApi() {
             var template = $('#music-template').html();
             var compiled = Handlebars.compile(template);
             var target = $('#target');
+            var select = $('#select');
+            select.trigger('change');
 
             var response = data.response;
             var success = data.success;
-            console.log(data);
             if (success) {
                 for (var i = 0; i < response.length; i++) {
                     var element = response[i];
@@ -27,12 +34,23 @@ function callApi() {
                         'year' : element.year
                     });
                     target.append(musicObj);
-                    console.log(element); 
                 }
+                select.change(function () {
+                    var data = $(this).val();
+                    for (var i = 0; i < response.length; i++) {
+                        var x = response[i].genre;
+                        y = x.toLowerCase();
+                        console.log(x);
+                        if (y === data) {
+                            element.show();
+                        } else {
+                            element.hide();
+                        }
+                    }
+                });
             } else {
                 console.log('error');
-            }  
-                
+            }     
         },
         error : function (error) {
                 
@@ -40,8 +58,4 @@ function callApi() {
         }
     });
 }
-
-
-
-
 $(document).ready(init);
